00132b479a59404e20dd72cd462a38eb617f13b2
ajn2004 <ajn2004@med.cornell.edu>
2018-06-26 10:07:51 -0400

    clean up commit

--- Single-release-codes/Difference_viewer.m
+++ Single-release-codes/Difference_viewer.m
@@ -3,15 +3,16 @@ clearvars;
 clc;
 
 % i1 = readtiff();
-files = dir('local*');
+files = dir('*tif');
 
 for i = 1:numel(files)
     i1 = readtiff(files(i).name);
     i2(:,:,i) = i1(:,:,2)-i1(:,:,1);
 i2(:,:,i) = (i2(:,:,i)>0).*i2(:,:,i);
-imagesc(i2(:,:,i));
-drawnow
-waitforbuttonpress
+i3(:,:,i) = rollingball(i2(:,:,i));
+% imagesc(i3(:,:,i));
+% drawnow
+% waitforbuttonpress
 end
 i2 = (i2>0).*i2;
 % imagesc(i2);
