bcc7bfa037289dd077efa6079b6bea0f8ee631b7
Andrew Nelson <33523882+ajn2004@users.noreply.github.com>
2021-02-18 06:56:38 -0500

    changed json converter
    Made it to automatically save data to shared dropbox folder w/ Tim

--- Functions/Misc/make_cdata_json.m
+++ Functions/Misc/make_cdata_json.m
@@ -1,9 +1,15 @@
 clearvars
-mkdir('jsons')
+base_folder = 'G:\Dropbox\Andrew & Tim shared folder\Analyzed Data\';
+data_folder = pwd;
+index = strfind(data_folder, 'Data');
+endex = strfind(data_folder, 'Analysis');
+
+new_save_folder = [base_folder, data_folder(index + 5:endex - 1)];
+mkdir(new_save_folder);
 files = dir('*avg.mat');
 
 for i = 1:numel(files)
     load(files(i).name,'cdata');
-    filename = ['jsons\',files(i).name(1:end-3),'json'];
+    filename = [new_save_folder,files(i).name(1:end-3),'json'];
     save_cdata_as_json(filename, cdata);
 end
