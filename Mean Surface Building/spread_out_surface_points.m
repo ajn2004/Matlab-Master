9efe897a1b1b69e08188218c6e6fe16cfcb51d02
ajn2004 <ajn2004@med.cornell.edu>
2021-11-16 13:48:56 -0500

    adding algos

---
+++ Mean Surface Building/spread_out_surface_points.m
@@ -0,0 +1,24 @@
+function spread_surface = spread_out_surface_points(dense_surface, kk, m, iterations)
+sdata = dense_surface;
+sdata1 = sdata;
+
+fixed = sdata(:,1) > max(sdata(:,1)) - 0.1 | sdata(:,1) < min(sdata(:,1)) + 0.1;
+free = find(fixed == 0);
+for i = 1:iterations
+    [Idx, D] = knnsearch(sdata,sdata(free,:),'K',kk);
+    for k = 1:numel(free)
+        sub_set = sdata(Idx(k,2:end),:);
+        r = sub_set - sdata(free(k),:);
+        rh = -r./abs(r);
+        coeff = pca(sub_set);
+        proj_mat = coeff(:,3).*coeff(:,3).';
+        orth_proj = (proj_mat*rh.').';
+        rallowed = rh - orth_proj;
+        kick = m*sum(rallowed);
+        sdata1(free(k),:) = sdata(free(k),:) + kick;
+        
+    end
+    sdata = sdata1;
+    
+end
+spread_surface = sdata;
--- Mean Surface Building/upsample_smooth_surface.m
+++ Mean Surface Building/upsample_smooth_surface.m
@@ -1,4 +1,4 @@
-function upsampled_surface = upsample_smooth_surface(smooth_surface,kk,shrink, iterations)
+function upsampled_surface = upsample_smooth_surface(smooth_surface,kk,shrink,m, iterations)
 
 [k, index] = knnsearch(smooth_surface,smooth_surface,'k',kk);
 for j = 1:iterations
@@ -10,18 +10,48 @@ for j = 1:iterations
         end
         upsampled_surface = [upsampled_surface; mean(smooth_surface(k(i,2:end),:))];
     end
-    
-    k = boundary(upsampled_surface,shrink);
-    for i = 1:numel(k(:,1))
-        upsampled_surface = [upsampled_surface; mean(upsampled_surface(k(i,:),:))];
+    upsampled_surface = unique(upsampled_surface,'rows');
+    smooth_surface = [smooth_surface; upsampled_surface]; 
+    for i = 1:3
+    smooth_surface(isnan(smooth_surface(:,i)),:) = [];
     end
-    smooth_surface = [smooth_surface; upsampled_surface];    
+    k = boundary(smooth_surface,shrink);
+    for i = 1:numel(k(:,1))
+        smooth_surface = [smooth_surface; mean(smooth_surface(k(i,:),:))];
+    end   
     [k, index] = knnsearch(smooth_surface,smooth_surface,'k',kk);
-%     for i = 1:numel(k(:,1))
-%         upsampled_surface = [upsampled_surface; mean(smooth_surface(k(i,[1,6:10]),:))];
-%     end
+
+    % knn 'push'
+%     ks = 3;
+    [Idx, d] = knnsearch(smooth_surface,smooth_surface,"K",kk);
+    push_surface = smooth_surface;
+
+    for i = 1:numel(Idx(:,1))
+        kick = [0,0,0];
+        for l = 1:3
+            for j = 2:kk
+                kick(l) =kick(l)- m*diff(smooth_surface(Idx(i,[1,j]),l))./abs(diff(smooth_surface(Idx(i,[1,j]),l)))^1;
+            end
+        end
+        push_surface(i,:) = push_surface(i,:)+ kick/kk;
+    end
+    for i = 1:3
+        push_surface(isnan(push_surface(:,i)),:) = [];
+    end
+    smooth_surface = push_surface; 
+    k = boundary(smooth_surface,shrink);
+    for i = 1:numel(k(:,1))
+        smooth_surface = [smooth_surface; mean(smooth_surface(k(i,:),:))];
+    end
+%     smooth_surface = upsampled_surface;
 end
-upsampled_surface = smooth_surface;
+
+k = boundary(smooth_surface,shrink);
+uk = unique(k);
+
+
+
+upsampled_surface = smooth_surface(uk,:);
 
 % Boundary Averaging Behavior
 % function upsampled_surface = upsample_smooth_surface(smooth_surface,shrink, iterations)
