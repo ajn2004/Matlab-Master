69e9332f98dc1d65671a99ff75e5ffb1539c824f
Andrew Nelson <33523882+ajn2004@users.noreply.github.com>
2021-11-29 07:42:12 -0500

    adjusts

--- Functions/Misc/g2c.m
+++ Functions/Misc/g2c.m
@@ -2,6 +2,6 @@ function g2c()
 try
 cd('C:\Users\AJN Lab\Documents\GitHub\Matlab-Master');
 catch
-    cd('C:\Users\andre\Documents\GitHub\Matlab-Master');
+    cd('C:\Users\ajnel\Documents\GitHub\Matlab-Master');
 end
 clc
